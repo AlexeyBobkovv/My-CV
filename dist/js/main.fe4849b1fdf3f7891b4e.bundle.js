6d7e7bde9a1f9fa35867ddc5fbc0ae9db611ace5
Just_Alexey <60109266+AlexeyBobkovv@users.noreply.github.com>
2022-02-07 14:40:21 +0300

    add: normal margin

--- dist/js/main.fe4849b1fdf3f7891b4e.bundle.js
+++ dist/js/main.fe4849b1fdf3f7891b4e.bundle.js
@@ -1,1 +1,22 @@
-"use strict";(self.webpackChunkwebpack_boilerplate=self.webpackChunkwebpack_boilerplate||[]).push([[179],{579:(e,c,l)=>{var t=l(694);document.querySelectorAll(".tree").forEach((function(e){console.log("🚀 ~ file: index.js ~ line 10 ~ htmlEl.forEach ~ el",e);var c=document.createElement("img");c.src=t,e.append(c)})),document.createElement("div").classList.add("image"),document.createElement("img").src="/assets/example.png"},694:(e,c,l)=>{e.exports=l.p+"55e3083127eb38262f5c.png"}},e=>{e(e.s=579)}]);
+'use strict'
+;(self.webpackChunkwebpack_boilerplate = self.webpackChunkwebpack_boilerplate || []).push([
+  [179],
+  {
+    579: (e, c, l) => {
+      var t = l(694)
+      document.querySelectorAll('.tree').forEach(function (e) {
+        console.log('🚀 ~ file: index.js ~ line 10 ~ htmlEl.forEach ~ el', e)
+        var c = document.createElement('img')
+        ;(c.src = t), e.append(c)
+      }),
+        document.createElement('div').classList.add('image'),
+        (document.createElement('img').src = '/assets/example.png')
+    },
+    694: (e, c, l) => {
+      e.exports = l.p + '55e3083127eb38262f5c.png'
+    },
+  },
+  (e) => {
+    e((e.s = 579))
+  },
+])
