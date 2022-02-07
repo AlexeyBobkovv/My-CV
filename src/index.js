e89733a8bd569729b504035b748551842f8c770c
Just_Alexey <60109266+AlexeyBobkovv@users.noreply.github.com>
2022-02-07 16:10:35 +0300

    add: favicon and links

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import tree from './images/tree.png'
 const htmlEl = document.querySelectorAll('.tree')
 
 htmlEl.forEach((el) => {
-  console.log('🚀 ~ file: index.js ~ line 10 ~ htmlEl.forEach ~ el', el)
   let treeEl = document.createElement('img')
   treeEl.src = tree
   el.append(treeEl)
